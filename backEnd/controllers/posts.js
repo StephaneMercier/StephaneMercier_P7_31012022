ae19966dd70d2eff53ea8d0b8c71ec497c30e830
StephaneMercier <stefane.mercier@gmail.com>
2022-02-20 17:51:07 +0100

    Ctrl Post done

---
+++ backEnd/controllers/posts.js
@@ -0,0 +1,72 @@
+const { User, Post } = require("../models");
+const fs = require("fs");
+
+// Create a Post
+exports.createPost = async (req, res, next) => {
+  try {
+    const userId = req.user.id;
+    const params = req.body;
+    const newPost = await Post.create({
+      userId,
+      body: params.body,
+    });
+
+    if (!newPost) {
+      throw new Error("Unable to create this post");
+    }
+    res.status(200).json(newPost);
+  } catch (e) {
+    res.status(400).json({ message: e.message });
+  }
+};
+
+// Get All Posts
+exports.getAllPosts = async (req, res, next) => {
+  try {
+    const allPosts = await Post.findAll({});
+    if (!allPosts) {
+      throw new Error("Unable to retrieve all posts");
+    }
+    res.status(200).send({ allPosts });
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+};
+
+// Update post
+exports.updatePost = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const findPostToUpdate = await Post.update({ where: { id } });
+    if (!findPostToUpdate) {
+      throw new Error("No post found");
+    }
+    if (findPostToUpdate && findPostToUpdate.UserId !== req.user.id) {
+      res.status(400).json({ message: e.message });
+    }
+    await findPostToUpdate.update({
+      body: req.body.body,
+      userId: req.user.id,
+    });
+    res.status(201).send({ postUpdate, message: "Post updated successfully" });
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+};
+
+// delete a Post
+exports.deletePost = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const findPost = await Post.findOne({ where: { id } });
+    if (
+      (findPost && findPost.userId == req.user.id) ||
+      req.user.isAdmin === true
+    ) {
+      Post.destroy({ where: { id: req.params.id } });
+    }
+    res.status(200).send({ message: "Successfully deleted the post" });
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+};
