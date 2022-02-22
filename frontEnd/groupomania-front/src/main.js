e8f5b230e690600c2436a1e39d00068170ebf653
StephaneMercier <stefane.mercier@gmail.com>
2022-02-22 16:27:35 +0100

    added disabled state condition on button

--- frontEnd/groupomania-front/src/main.js
+++ frontEnd/groupomania-front/src/main.js
@@ -6,7 +6,7 @@ import store from "./store";
 
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import "bootstrap/dist/css/bootstrap.css";
-// import "bootstrap-vue/dist/bootstrap-vue.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
 // Make BootstrapVue available throughout your project
 // Vue.use(BootstrapVue);
--- frontEnd/groupomania-front/src/views/Login.vue
+++ frontEnd/groupomania-front/src/views/Login.vue
@@ -4,38 +4,77 @@
       <h1 class="card-title" v-if="mode == `login`">Se connecter</h1>
       <h1 class="card-title" v-else>S'inscrire</h1>
       <p class="card-subtitle" v-if="mode == `login`">
-        Pas encore de compte ?
+        (Pas encore de compte ?
         <span class="card-action" @click="switchToSignup()"
-          >Cliquez ici pour vous inscrire</span
+          >Cliquez ici pour vous inscrire)</span
         >
       </p>
       <p class="card-subtitle" v-else>
-        Déjà Inscrit ?
+        (Déjà Inscrit ?
         <span class="card-action" @click="switchToLogin()"
-          >Cliquez ici pour vous connecter</span
+          >Cliquez ici pour vous connecter)</span
         >
       </p>
       <form action="submit" v-if="mode == 'login'">
-        <input type="email" class="form-control" placeholder="E-mail :" />
+        <input
+          id="email"
+          v-model="email"
+          type="email"
+          class="form-control"
+          placeholder="E-mail :"
+          required
+        />
         <input
           type="password"
+          v-model="password"
           class="form-control"
           placeholder="Mot de Passe :"
+          required
         />
-        <button type="submit" class="btn btn-primary">Connexion</button>
+        <br />
+        <button class="btn btn-primary" :disabled="switchToDisabled()">
+          Connexion
+        </button>
       </form>
 
       <form action="submit" v-else>
-        <input type="text" class="form-control" placeholder="Nom :" />
-        <input type="text" class="form-control" placeholder="Prénom :" />
-        <input type="email" class="form-control" placeholder="E-mail :" />
         <input
+          id="name"
+          v-model="name"
+          type="text"
+          class="form-control"
+          placeholder="Nom :"
+          required
+        />
+        <input
+          id="lastName"
+          v-model="lastName"
+          type="text"
+          class="form-control"
+          placeholder="Prénom :"
+          required
+        />
+        <input
+          id="email"
+          v-model="email"
+          type="email"
+          class="form-control"
+          placeholder="E-mail :"
+          required
+        />
+        <input
+          id="password"
+          v-model="password"
           type="password"
           class="form-control"
           placeholder="Mot de passe :"
+          required
         />
-        <button class="btn btn-primary">Inscription</button>
-        <button class="btn btn-secondary">Recommencer</button>
+        <br />
+
+        <button class="btn btn-primary" :disabled="switchToDisabled()">
+          C'est parti !
+        </button>
       </form>
     </div>
   </div>
@@ -60,6 +99,19 @@ export default {
     },
     switchToLogin: function () {
       this.mode = "login";
+    },
+    switchToDisabled: function () {
+      if (this.mode == "signup") {
+        if (
+          this.email == "" ||
+          this.password == "" ||
+          this.name == "" ||
+          this.lastName == ""
+        )
+          return true;
+      } else if (this.mode == "login") {
+        if (this.email == "" || this.password == "") return true;
+      }
     },
   },
 };
